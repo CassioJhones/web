bf8a3f9be6f044f4c6c6455e14afa8f56c4006a4
CassioJhones <cassio.bjhones@gmail.com>
2025-03-04 18:37:02 -0300

    fontes e cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-white">Hello world!</div>
+      <h1 className="text-4xl font-semibold font-heading">Titulo</h1>
+<Button text='Cassio'/>
+<Button text="Roxo"/>
+
     </main>
   );
 }
